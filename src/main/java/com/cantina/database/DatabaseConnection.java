fc43efc9fa505fca3cb649586246839372069460
HermesRian <rianfoz@hotmail.com>
2025-07-02 20:23:41 -0300

    AQUI É O BACKEND

--- src/main/java/com/cantina/database/DatabaseConnection.java
+++ src/main/java/com/cantina/database/DatabaseConnection.java
@@ -7,8 +7,9 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/cantina_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "candinho2020";
+    private static final String USER = System.getenv("user_db");
+    private static final String PASSWORD = System.getenv("password_db");
+
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
